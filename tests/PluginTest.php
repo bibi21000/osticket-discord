8c22a26ba4c00c6b85542b42eb6acf84b766f4e4
bibi21000 <bibi21000@gmail.com>
2019-03-22 00:11:41 +0100

    add tests

---
+++ tests/PluginTest.php
@@ -0,0 +1,24 @@
+<?php
+require_once('../upload/include/plugins'. 'class.format.php');
+
+class PluginTests extends PHPUnit_Framework_TestCase
+{
+    private $calculator;
+ 
+    protected function setUp()
+    {
+        $this->plugin = new DiscordPlugin();
+    }
+ 
+    protected function tearDown()
+    {
+        $this->plugin = NULL;
+    }
+ 
+    public function testBootstrap()
+    {
+        $result = $this->plugin->bootstrap();
+        $this->assertEquals(3, $result);
+    }
+ 
+}
